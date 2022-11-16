b480a7f28175140c2c838a7ceceed210431bc120
Simone Fortunato <frtsmn94@gmail.com>
2022-11-16 09:40:50 +0100

    first commit

---
+++ src/org/generation/italy/StampaNumeri.java
@@ -0,0 +1,21 @@
+package org.generation.italy;
+import java.util.Scanner;
+
+public class StampaNumeri {
+	public static void main(String[] Args) {
+		
+		Scanner in = new Scanner(System.in);
+		System.out.println("Inserisci un numero");
+		
+		int numero = in.nextInt();
+		
+		if (numero %2 == 0) {
+			System.out.println(numero);
+		}else {
+			System.out.println(numero + 1 );
+		}
+		
+		
+	}
+	
+}
